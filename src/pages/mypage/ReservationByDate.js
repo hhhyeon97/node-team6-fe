10632a2375276bee53a6a2def01858c87375fc71
jebi2420 <jebi2420@gmail.com>
2024-06-20 23:21:37 +0900

    [docs] 파일 경로 수정

--- src/pages/mypage/ReservationByDate.js
+++ src/pages/mypage/ReservationByDate.js
@@ -1,11 +1,11 @@
 import React from "react";
-import MyPageLayout from "../Layout/MyPageLayout";
+import MyPageLayout from '../../Layout/MyPageLayout';
 
 // 예약날짜로 나의 예매 조회 컴포넌트
 const ReservationByDate = () => {
     return (
         <MyPageLayout>
-            <div>예약날짜로 나의 예매 조회 페이지</div>;
+            <div>예약날짜로 나의 예매 조회 페이지</div>
         </MyPageLayout>
     )
 }
