901825ae5b822c0d278c458a58236d9eebc243e4
jebi2420 <jebi2420@gmail.com>
2024-06-20 22:39:27 +0900

    [chore] mypage router 설정

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, Navigate } from 'react-router';
 // import PrivateRoute from "./PrivateRoute";
 import ListPage from '../pages/ListPage';
 import RegisterPage from '../pages/RegisterPage';
 import PerformanceDetail from '../pages/PerformanceDetail';
 import LoginPage from '../pages/LoginPage';
+import MyPageLayout from '../Layout/AppLayout'
+import ReservationByDate from '../pages/mypage/ReservationByDate'
+import ViewAllReservations from "../pages/mypage/ViewAllReservations";
+import MyReviews from "../pages/mypage/MyReviews";
+import EditProfile from "../pages/mypage/EditProfile";
 
 const AppRouter = () => {
   return (
@@ -14,6 +19,13 @@ const AppRouter = () => {
       <Route path="/performance/:id" element={<PerformanceDetail />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/mypage" element={<MyPageLayout />}>
+        <Route index element={<Navigate to="reservations/by-date" />} />
+        <Route path="reservations/by-date" element={<ReservationByDate />} />
+        <Route path="reservations/view-all" element={<ViewAllReservations />} />
+        <Route path="reviews" element={<MyReviews />} />
+        <Route path="edit-profile" element={<EditProfile />} />
+      </Route>
     </Routes>
   );
 };
