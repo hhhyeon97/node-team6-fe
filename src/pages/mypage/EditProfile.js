d8cca41e566e6faa2003f889bd157217a22692ba
jebi2420 <jebi2420@gmail.com>
2024-06-21 17:16:34 +0900

    [chore] 각 페이지마다 title에  props 던져주기

--- src/pages/mypage/EditProfile.js
+++ src/pages/mypage/EditProfile.js
@@ -4,7 +4,7 @@ import MyPageLayout from '../../Layout/MyPageLayout';
 // 회원정보 수정 컴포넌트
 const EditProfile = () => {
     return (
-        <MyPageLayout>
+        <MyPageLayout title="나의 계정" cap="회원정보 수정">
             <div>회원정보 수정 페이지</div>
         </MyPageLayout>
     )
