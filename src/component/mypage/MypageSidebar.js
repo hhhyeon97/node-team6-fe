db8954f57b0ad9a018941f565d0dcfd5da688b26
jebi2420 <jebi2420@gmail.com>
2024-06-21 16:57:32 +0900

    [chore] 사이드바 내용 수정

--- src/component/mypage/MypageSidebar.js
+++ src/component/mypage/MypageSidebar.js
@@ -7,17 +7,17 @@ const MypageSidebar = () => {
   return(
     <div className='mypage_sidebar'>
       <ul>
-        <h3>나의 예매정보</h3>
+        <h3>나의 예매</h3>
           <ul className='link_menu'>
             <li><Link to="/mypage/reservations/by-date">예약날짜로 조회</Link></li>
             <li><Link to="/mypage/reservations/view-all">전체 조회</Link></li>
           </ul>
-        <h3>나의 활동정보</h3>
+        <h3>나의 활동</h3>
           <ul className='link_menu'>
           <li><Link to="/mypage/reviews">나의 리뷰</Link></li>
           <li><Link to="/mypage/reviews">나의 찜</Link></li>
           </ul>
-        <h3>나의 계정설정</h3>
+        <h3>나의 계정</h3>
           <ul className='link_menu'>
             <li><Link to="/mypage/edit-profile">회원정보 수정</Link></li>
           </ul>
