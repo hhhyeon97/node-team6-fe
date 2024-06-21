9182caafa734122142c033aa0016d84fda38ea24
jebi2420 <jebi2420@gmail.com>
2024-06-21 20:25:02 +0900

    [feat] getUserList action 추가

--- src/Layout/AdminPageLayout.js
+++ src/Layout/AdminPageLayout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Col, Row, Container } from "react-bootstrap";
 
-const AdminPageLayout = () => {
+const AdminPageLayout = ({ children }) => {
 
   return (
     <Container className='wrap-container'>
-      <h1>admin page</h1>
+      <div className="admin-page-container" style={{ marginTop: '20px', marginBottom: '20px', border: '1px solid #ccc' }}>
+        <h1>admin page</h1>
+        <main>{children}</main>
+      </div>
     </Container>
   );
 }
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -3,6 +3,7 @@ const initialState = {
   loading: false,
   user: null,
   error: '',
+  userList: [],
 };
 
 function userReducer(state = initialState, action) {
@@ -11,13 +12,17 @@ function userReducer(state = initialState, action) {
     case types.REGISTER_USER_REQUEST:
     case types.LOGIN_REQUEST:
     case types.LOGIN_WITH_TOKEN_REQUEST:
+    case types.GET_USER_LIST_REQUEST:
       return { ...state, loading: true };
     case types.LOGIN_SUCCESS:
     case types.LOGIN_WITH_TOKEN_SUCCESS:
       return { ...state, loading: false, user: payload.user };
+    case types.GET_USER_LIST_SUCCESS:
+      return { ...state, loading: false, userList: payload };
     case types.REGISTER_USER_FAIL:
     case types.LOGIN_FAIL:
     case types.LOGIN_WITH_TOKEN_FAIL:
+    case types.GET_USER_LIST_FAIL:
       return { ...state, loading: false, error: payload };
     case types.RESET_ERROR:
       return {
