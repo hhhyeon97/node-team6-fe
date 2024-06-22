9a8654a99fec4c6e6bb5f8e6dfda0aa8053a5369
jin <jin200126@gmail.com>
2024-06-23 02:46:31 +0900

    address

--- src/component/KaKaoMap.js
+++ src/component/KaKaoMap.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 const { kakao } = window
 
 const KaKaoMap = ({ lat, lot }) => {
+    const [address, setAddress] = useState('')
 
     useEffect(() => {
         const mapContainer = document.getElementById('map'), // 지도를 표시할 div 
@@ -11,7 +12,7 @@ const KaKaoMap = ({ lat, lot }) => {
             };
 
         const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
+        var geocoder = new kakao.maps.services.Geocoder();
         // 마커가 표시될 위치입니다 
         const markerPosition = new kakao.maps.LatLng(lat, lot);
 
@@ -20,12 +21,23 @@ const KaKaoMap = ({ lat, lot }) => {
             position: markerPosition
         });
 
+        var callback = function (result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+                setAddress(result[0].address.address_name);
+            }
+        };
+
+        geocoder.coord2Address(markerPosition.getLng(), markerPosition.getLat(), callback);
+
         // 마커가 지도 위에 표시되도록 설정합니다
         marker.setMap(map);
     }, [])
 
     return (
-        <div id="map" style={{ width: '500px', height: '400px' }}></div>
+        <div>
+            <div>{address}</div>
+            <div id="map" style={{ width: '500px', height: '400px' }}></div>
+        </div>
     )
 }
 
