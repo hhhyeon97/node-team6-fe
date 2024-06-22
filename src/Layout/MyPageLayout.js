77bc152ea49a3753df462299da90eaeda846aa42
jebi2420 <jebi2420@gmail.com>
2024-06-22 18:53:32 +0900

    [chore] 컴포넌트 이름 변경에 따른 import 수정

--- src/Layout/MyPageLayout.js
+++ src/Layout/MyPageLayout.js
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row, Container } from "react-bootstrap";
 import MypageUserLevel from '../component/mypage/MypageUserLevel';
 import MypageSidebar from "../component/mypage/MypageSidebar";
-import MypageUserProfile from "../component/mypage/MypageUserProfile";
+// import MypageUserProfile from "../component/mypage/MypageUserProfile";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../style/css/Mypage.css";
 import LinedTitle from '../component/LinedTitle';
+import UserProfile from '../component/UserProfile';
 
 const MyPageLayout = ({ title, cap, children }) => {
   // const { user } = useSelector((state) => state.user);
@@ -16,7 +17,8 @@ const MyPageLayout = ({ title, cap, children }) => {
     <Container className='wrap-container'>
       <div className="mypage-container"style={{ marginTop: '20px', marginBottom: '20px', border: '1px solid #ccc' }}>
         <section className="mypage-left">
-          <MypageUserProfile />
+          {/* <MypageUserProfile /> */}
+          <UserProfile />
           <MypageSidebar />
         </section>
         <section className="mypage-right">
