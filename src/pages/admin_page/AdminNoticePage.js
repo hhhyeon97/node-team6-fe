8d7b0d635fa995787f3b87b683465769985ba293
jebi2420 <jebi2420@gmail.com>
2024-06-22 18:15:03 +0900

    [chore] admin 페이지들 생성

--- src/pages/admin_page/AdminNoticePage.js
+++ src/pages/admin_page/AdminNoticePage.js
@@ -5,24 +5,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { userActions } from '../../action/userAction';
 import LinedTitle from '../../component/LinedTitle';
 
-const AdminUserPage = () => {
+const AdminNoticePage = () => {
   const dispatch = useDispatch();
-  const { userList } = useSelector((state) => state.user);
+  // const { userList } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    dispatch(userActions.getUserList());
-  }, [dispatch]);
-  console.log("userList", userList)
+  // useEffect(() => {
+  //   dispatch(userActions.getUserList());
+  // }, [dispatch]);
+  // console.log("userList", userList)
+
   return(
     <AdminPageLayout>
-      <LinedTitle title='회원관리' cap='회원 정보를 조회하고 회원등급을 수정할 수 있습니다'/>
+      <LinedTitle title='공지사항관리' cap='공지사항을 작성하고 관리할 수 있습니다'/>
       <ul>
-        {userList.map((user) => (
+        {/* {userList.map((user) => (
           <li key={user.id}>{user.name}</li>
-        ))}
+        ))} */}
       </ul>
     </AdminPageLayout>
   );
 }
 
-export default AdminUserPage;
+export default AdminNoticePage;
--- src/pages/admin_page/AdminReviewPage.js
+++ src/pages/admin_page/AdminReviewPage.js
@@ -5,24 +5,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { userActions } from '../../action/userAction';
 import LinedTitle from '../../component/LinedTitle';
 
-const AdminUserPage = () => {
+const AdminReviewPage = () => {
   const dispatch = useDispatch();
-  const { userList } = useSelector((state) => state.user);
+  // const { userList } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    dispatch(userActions.getUserList());
-  }, [dispatch]);
-  console.log("userList", userList)
+  // useEffect(() => {
+  //   dispatch(userActions.getUserList());
+  // }, [dispatch]);
+  // console.log("userList", userList)
+
   return(
     <AdminPageLayout>
-      <LinedTitle title='회원관리' cap='회원 정보를 조회하고 회원등급을 수정할 수 있습니다'/>
+      <LinedTitle title='리뷰관리' cap='회원들이 남긴 리뷰를 조회하고 리뷰의 상태를 수정할 수 있습니다'/>
       <ul>
-        {userList.map((user) => (
+        {/* {userList.map((user) => (
           <li key={user.id}>{user.name}</li>
-        ))}
+        ))} */}
       </ul>
     </AdminPageLayout>
   );
 }
 
-export default AdminUserPage;
+export default AdminReviewPage;
