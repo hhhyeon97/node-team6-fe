9ddf67bc0108ca62ecabeddeef066a5ebe847421
yisoo song <yisoo1049@gmail.com>
2024-06-23 01:09:37 +0900

    [test]라이브컴파일러테스트

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { userActions } from '../action/userAction';
 import { useDispatch } from 'react-redux';
-import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
-import '../style/Navbar.css';
-import { faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
+// import '../style/Navbar2.css';
+import '../style/css/Navbar.css'
+import { faDoorOpen, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Container } from 'react-bootstrap';
 const Navbar = ({ user }) => {
   const dispatch = useDispatch();
   let navigate = useNavigate();
@@ -23,39 +24,41 @@ const Navbar = ({ user }) => {
   };
 
   return (
-    <div className="nav_area">
-      <div className="nav_logo_area">
-        <a href="/">
-          <img src="/testImage/logo.png" />
-        </a>
+    <Container className='wrap-container'>
+      <div className="nav_area">
+        <div className="nav_logo_area">
+          <a href="/">
+            <img src="/testImage/logo.png" />
+          </a>
+        </div>
+        <div className="nav_menu">
+          <div><FontAwesomeIcon icon={faUser} /> {user?'MY PAGE':'JOIN US'}</div>
+          <div><FontAwesomeIcon icon={faHeart} /> {user?'':'MY LIKE'}</div>
+          {user ? (
+            <div onClick={logout} className="nav_icon">
+              <FontAwesomeIcon icon={faDoorOpen} />
+              <span>LOGOUT</span>
+            </div>
+          ) : (
+            <div onClick={() => navigate('/login')} className="nav_icon">
+              <FontAwesomeIcon icon={faDoorOpen} />
+              <span>LOGIN</span>
+            </div>
+          )}
+        </div>
+        <div>
+          <ul className='nav_category'>
+            {menuList.map((menu,index)=>(
+              <li key={index}>
+                <button>
+                  {menu}
+                </button>
+                </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="nav_menu">
-        <div><FontAwesomeIcon icon={faUser} /> {user?'MY PAGE':'JOIN US'}</div>
-        <div><FontAwesomeIcon icon={faHeart} /> {user?'':'MY LIKE'}</div>
-        {user ? (
-          <div onClick={logout} className="nav_icon">
-            <FontAwesomeIcon icon={faCircleUser} />
-            <span>LOGOUT</span>
-          </div>
-        ) : (
-          <div onClick={() => navigate('/login')} className="nav_icon">
-            <FontAwesomeIcon icon={faCircleUser} />
-            <span>LOGIN</span>
-          </div>
-        )}
-      </div>
-      <div>
-        <ul className='nav_category'>
-          {menuList.map((menu,index)=>(
-            <li key={index}>
-              <button>
-                {menu}
-              </button>
-              </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </Container>
   );
 };
 
