e3487bf8a4f73e2d4136c5f1173e75d7b0b9c36a
jebi2420 <jebi2420@gmail.com>
2024-06-22 18:54:41 +0900

    [feat] 회원관리 페이지에 user 리스트 뿌리기

--- src/pages/admin_page/AdminUserPage.js
+++ src/pages/admin_page/AdminUserPage.js
@@ -13,14 +13,23 @@ const AdminUserPage = () => {
     dispatch(userActions.getUserList());
   }, [dispatch]);
   console.log("userList", userList)
+
   return(
     <AdminPageLayout>
-      <LinedTitle title='회원관리' cap='회원 정보를 조회하고 회원등급을 수정할 수 있습니다'/>
-      <ul>
-        {userList.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <div className="admin_user_container">
+        <LinedTitle title='회원관리' cap='회원 정보를 조회하고 회원등급을 수정할 수 있습니다'/>
+        <ul>
+          {userList.map((user) => (
+            <li key={user.id} className='user_item'>
+              <div>{user.name}</div>
+              <div>{user.level}</div>
+              <div>{user.email}</div>
+              <div>{user.contact}</div>
+              <div>{user.createdAt}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </AdminPageLayout>
   );
 }
