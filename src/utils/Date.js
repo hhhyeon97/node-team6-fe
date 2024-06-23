76434a4d5bdbff140d09a4cfc0f12b62161f7a3a
jebi2420 <jebi2420@gmail.com>
2024-06-24 00:13:51 +0900

    [feat] 한국시간 변환 함수 생성

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -60,5 +60,23 @@ const EndDateformat = (value) => {
 
 }
 
+const convertToKST = (utcDate) => {
+    if(utcDate){
 
-export { Dateformat, StringDateformat, EndDateformat };
+      const date = new Date(utcDate);
+      const kstOffset = 9 * 60 * 60 * 1000; // UTC+9
+      const kstDate = new Date(date.getTime() + kstOffset);
+      kstDate.toISOString().replace('T', ' ').substring(0, 19); // 한국 시간 형식으로 변경
+  
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+  
+      const formattedDate = `${year}.${month}.${day}`;
+  
+      return formattedDate
+    }
+    return null
+  };
+
+export { Dateformat, StringDateformat, EndDateformat, mongodbTime, convertToKST };
