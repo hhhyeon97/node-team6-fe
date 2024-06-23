ac65ba48718fb75c99fdb65ad91771afd072f78a
jebi2420 <jebi2420@gmail.com>
2024-06-23 20:43:23 +0900

    [feat] 리뷰 리스트 가져오기 및 필요한 파일 생성

--- src/pages/admin_page/AdminReviewPage.js
+++ src/pages/admin_page/AdminReviewPage.js
@@ -1,27 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import { Col, Row, Container } from "react-bootstrap";
 import AdminPageLayout from '../../Layout/AdminPageLayout';
 import { useDispatch, useSelector } from "react-redux";
-import { userActions } from '../../action/userAction';
+import { reviewActions } from '../../action/reviewAction';
 import LinedTitle from '../../component/LinedTitle';
 
 const AdminReviewPage = () => {
   const dispatch = useDispatch();
-  // const { userList } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const { reviewList, totalPageNum } = useSelector((state) => state.review);
+  const [query, setQuery] = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState({
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  });
 
-  // useEffect(() => {
-  //   dispatch(userActions.getUserList());
-  // }, [dispatch]);
-  // console.log("userList", userList)
+  // [ reviewList 가져오기 ]
+  useEffect(() => {
+    dispatch(reviewActions.getReviewList({ ...searchQuery }));
+  }, [query, dispatch]);
+
+  console.log("reviewList", reviewList )
+  console.log("reviewPage", totalPageNum)
+
+  // [ 검색어나 페이지가 바뀌면 url바꿔주기 ]
+  useEffect(() => {
+    if(searchQuery === ""){
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate("?" + query);
+  }, [searchQuery]);
+
+  // [ 쿼리에 페이지값 바꿔주기 ]
+  const onPageChange = ({ selected }) => {
+    setSearchQuery({...searchQuery, page: selected +1});
+  };
 
   return(
     <AdminPageLayout>
+      <div className='admin_review_container'>
       <LinedTitle title='리뷰관리' cap='회원들이 남긴 리뷰를 조회하고 리뷰의 상태를 수정할 수 있습니다'/>
-      <ul>
-        {/* {userList.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))} */}
-      </ul>
+
+      </div>
     </AdminPageLayout>
   );
 }
