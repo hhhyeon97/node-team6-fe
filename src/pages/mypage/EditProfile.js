f20b52384f54aba03bbc7632f89ddbd1e498c87d
jebi2420 <jebi2420@gmail.com>
2024-06-24 16:06:42 +0900

    [chore] 회원 정보 form 작업 중

--- src/pages/mypage/EditProfile.js
+++ src/pages/mypage/EditProfile.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from '../../action/userAction';
 import UserProfile from '../../component/UserProfile';
 import { Form, Col } from 'react-bootstrap';
+import CloudinaryUploadWidget from "../../utils/CloudinaryUploadWidget";
 
   // 초기 폼 데이터 설정
   let initialFormData = {
@@ -17,43 +18,102 @@ import { Form, Col } from 'react-bootstrap';
 const EditProfile = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState({ ...initialFormData });
-  
-  // [ user loading ]
-  if (!user) {
-    return <div>Loading...</div>; // 또는 다른 적절한 로딩 표시
-  }
+  const [emailError, setEmailError] = useState('');
+  const [formData, setFormData] = useState({
+    image:'',
+    name: '',
+    email: '',
+    contact: null
+  });
 
-  // 초기 폼 데이터 설정
-  const defaultForm = () => {
-    setFormData ={
-      name: user.name || '', // user가 undefined인 경우를 대비하여 기본값 '' 설정
-      email: user.email || '', // user가 undefined인 경우를 대비하여 기본값 '' 설정
-      contact: user.contact || '', // user가 undefined인 경우를 대비하여 기본값 '' 설정
+  // [ 유저 정보 받아오기 ]
+  useEffect(() => {
+    dispatch(userActions.getUser());
+  }, [dispatch]);
+
+  // [ user가 로딩되면 초기 데이터 설정 ]
+  useEffect(() => {
+    if (user) {
+      setFormData({
+        image: user.image || '',
+        name: user.name || '',
+        email: user.email || '',
+        contact: user.contact || null
+      });
     }
-  }
+  }, [user]);
 
   // [ form 요소 변화처리 ]
   const handleChange = (event) => {
     event.preventDefault();
     const { id, value } = event.target;
     setFormData({ ...formData, [id]: value});
+    console.log('formData', formData)
+  };
+
+  // [ 이미지 업로드 ]
+  const uploadImage = (url) => {
+    setFormData({...formData, image: url});
   };
   
   return (
     <MyPageLayout title="나의 계정" cap="회원정보 수정">
         <div>
           <Form className='edit_user_form_container'>
-            <Form.Group as={Col} controlId="name">
-            <Form.Label>이름</Form.Label>
-            <Form.Control
-              onChange={handleChange}
-              type="text"
-              placeholder="2자이상 10자 이하로 입력해주세요"
-              required
-              defaultValue={formData.name}
-            />
+          <Form.Group as={Col} controlId="image">
+              <Form.Label>프로필 이미지</Form.Label>
+              <CloudinaryUploadWidget uploadImage={uploadImage} />
+              <img
+                id="uploadedimage"
+                src={formData.image}
+                className="upload-image mt-2"
+                alt="uploadedimage"
+              ></img>
           </Form.Group>
+          
+          <Form.Group as={Col} controlId="image">
+              <Form.Label>이메일</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="2자이상 10자 이하로 입력해주세요"
+                required
+                defaultValue={formData.email}
+                />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="name">
+              <Form.Label>이름</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="2자이상 10자 이하로 입력해주세요"
+                required
+                defaultValue={formData.name}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="name">
+              <Form.Label>이메일</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="2자이상 10자 이하로 입력해주세요"
+                required
+                defaultValue={formData.email}
+                />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="name">
+              <Form.Label>연락처</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="2자이상 10자 이하로 입력해주세요"
+                required
+                defaultValue={formData.contact}
+                />
+            </Form.Group>
 
           </Form>
         </div>
