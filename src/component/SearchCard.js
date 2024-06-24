ff04f56a5b9dc518a6ca63768eaf953822df2bdf
yisoo song <yisoo1049@gmail.com>
2024-06-24 23:03:13 +0900

    [fix]디테일페이지수정

--- src/component/SearchCard.js
+++ src/component/SearchCard.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
 import { EndDate, StartDate } from '../utils/MainCartDate'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const SearchCard = ({item}) => {
+  const navigate = useNavigate();
   return (
     <Col lg={3}>
         <div className='search_card_area'>
-            <div className='search_card_img_box'>
+            <div className='search_card_img_box' onClick={()=>navigate(`/performance/${item.mt20id}`)}>
                 <img src={item.poster} className='search_card_img'/>
             </div>
-            <div className='search_card_text'>
+            <div className='search_card_text' onClick={()=>navigate(`/performance/${item.mt20id}`)}>
                 <h3>{item.prfnm}</h3>
                 <h4>{item.fcltynm}</h4>
                 <h5>{StartDate(item.prfpdfrom)} ~ {EndDate(item.prfpdto)}</h5>
--- src/component/mainpage/PerformanceCard.js
+++ src/component/mainpage/PerformanceCard.js
@@ -7,11 +7,11 @@ import { useNavigate } from 'react-router-dom'
 const PerformanceCard = ({item,key}) => {
     const navigate = useNavigate()
   return (
-    <div key={key} className='performance_card_area' onClick={()=>navigate(`/performance/${item.mt20id}`)}>
-        <div className='performance_card_img_box'>
+    <div key={key} className='performance_card_area'>
+        <div className='performance_card_img_box' onClick={()=>navigate(`/performance/${item.mt20id}`)}>
             <img src={item.poster} className='performance_card_img'/>
         </div>
-        <div>
+        <div onClick={()=>navigate(`/performance/${item.mt20id}`)}>
             <h3>{item.prfnm}</h3>
             <h4>{item.fcltynm}</h4>
             <h5>{StartDate(item.prfpdfrom)} ~ {EndDate(item.prfpdto)}</h5>
