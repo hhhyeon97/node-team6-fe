f8c84a493c028a54b15ecb60404e334da570681c
jebi2420 <jebi2420@gmail.com>
2024-06-24 14:50:27 +0900

    [chore] 회원 정보에 유저정보 반영 및 스타일 다듬기

--- src/component/UserProfile.js
+++ src/component/UserProfile.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
-const UserProfile = () => {
+const UserProfile = ({ user }) => {
 
   return(
     <div className='user_profile_container'>
       <div class="user_profile">
         <div className="user_img">
           {/* 토큰으로 user정보 가져오는거 되면 수정 */}
-          <FontAwesomeIcon icon={faCircleUser} /> 
+          {user.image ? (
+            <img 
+              src={user.image}
+              alt={user.name} 
+              style={{ width: '75px', height: '75px', objectFit: 'cover' }} 
+              onError={(e) => e.target.src = 'https://iconspng.com/_next/image?url=https%3A%2F%2Ficonspng.com%2Fimages%2Fabstract-user-icon-3%2Fabstract-user-icon-3.jpg&w=1080&q=75'}
+            />
+          ) : (
+            <img 
+              src='https://iconspng.com/_next/image?url=https%3A%2F%2Ficonspng.com%2Fimages%2Fabstract-user-icon-3%2Fabstract-user-icon-3.jpg&w=1080&q=75'
+              alt={user.name} 
+              style={{ width: '75px', height: '75px', objectFit: 'cover' }} 
+            />
+          )}
         </div>
-        <div className='user_name'>김제비</div>
+        <div className='user_name'>{user.name}</div>
       </div>
     </div>
   )
