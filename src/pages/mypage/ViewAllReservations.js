b77691384596ec15f5274e2f11e08db1a163dd51
jebi2420 <jebi2420@gmail.com>
2024-06-25 13:15:02 +0900

    [feat] 나의예매 내역 모두 가져오기

--- src/pages/mypage/ViewAllReservations.js
+++ src/pages/mypage/ViewAllReservations.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { useEffect } from 'react';
 import MyPageLayout from '../../Layout/MyPageLayout';
+import { reservationAction } from '../../action/reservationAction';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+
 
 // 나의 예매 모두 보기 컴포넌트
 const ViewAllReservations = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	// [ 나의예매 정보 받아오기 ]
+	useEffect(() => {
+		dispatch(reservationAction.getMyReserve());
+	}, [dispatch]);
+
     return (
         <MyPageLayout title="나의 예매" cap="전체 조회">
             <div>나의 예매 모두 조회 페이지</div>
--- src/reducer/reservationReducer.js
+++ src/reducer/reservationReducer.js
@@ -4,6 +4,7 @@ const initialState = {
     loading: false,
     reservationId: null,
     error: null,
+    reserveList: [],
 }
 
 function reservationReducer(state = initialState, action) {
@@ -11,14 +12,17 @@ function reservationReducer(state = initialState, action) {
 
     switch (type) {
         case types.POST_RESERVATION_REQUEST:
+        case types.GET_MY_RESERVATION_REQUEST:
             return { ...state, loading: true }
 
         case types.POST_RESERVATION_SUCCESS:
             return { ...state, loading: false, reservationId: payload, error: null }
+        case types.GET_MY_RESERVATION_SUCCESS:
+            return { ...state, loading: false, reserveList: payload }
 
         case types.POST_RESERVATION_FAIL:
+        case types.GET_MY_RESERVATION_FAIL:
             return { ...state, loading: false, error: payload }
-
         default:
             return state
     }
