54291fc04cd5892acc9b878d09e34891e87f44ee
jebi2420 <jebi2420@gmail.com>
2024-06-26 12:20:41 +0900

    [chore] 리뷰버튼 작업중

--- src/component/mypage/MyReserveList.js
+++ src/component/mypage/MyReserveList.js
@@ -9,43 +9,30 @@ import MyReserveCard from './MyReserveCard';
 const MyReserveList = ({ reserveList, openReviewForm }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { reviewedReserve } = useSelector((state) => state.review);
-  const { loading } = useSelector((state) => state.review);
-  // const [reviewedReserves, setReviewedReserves] = useState({});
-  const [isReviewed, setIsReviewed] = useState(false);
-  // const [reviewStatus, setReviewStatus] = useState({});
+  const { reviewedReserve, loading } = useSelector((state) => state.review);
+  
 
-  // useEffect(() => {
-  //   reserveList?.forEach(async (item) => {
-  //     dispatch(reviewAction.checkReviewed(item.ticket.SeqTitle, item._id));
-  //   })
-  // }, [reserveList]) 
-
+  // [ 각 예약 항목에 대해 리뷰 여부 확인 ]
   useEffect(() => {
-    // 각 예약 항목에 대해 리뷰 여부 확인
     reserveList?.forEach((item) => {
       dispatch(reviewAction.checkReviewed(item.ticket.SeqTitle, item._id));
-      setIsReviewed(reviewedReserve[item._id]);
     });
   }, [dispatch, reserveList]);
 
   console.log("이거뭐야",reviewedReserve)
 
-  if (loading) {
-    return <div>Loading...</div>; // 로딩 중일 때 보여줄 화면
-  }
   return(
     <div>
       {reserveList?.map((item, index) => (
-        <div>
-        <div>{item.ticket.SeqTitle}의 리뷰는 {isReviewed ? (<p>리뷰됨</p>) : (<p>리뷰안됨</p>)}</div>
+        <div key={item._id}>
+          <div>{item.ticket.SeqTitle}의 리뷰는 {reviewedReserve[item._id] ? (<p>리뷰됨</p>) : (<p>리뷰안됨</p>)}</div>
 
           <MyReserveCard 
             item={item}
             openReviewForm={openReviewForm}
             // isReviewed={reviewStatus[item._id]}
           />
-      {!isReviewed ? (
+      {!reviewedReserve[item._id] ? (
         <Button size="sm" onClick={() => openReviewForm(item)}>
           리뷰쓰기
         </Button>
