bb39226b3ccba46ca86b29445ac0421c1f30583c
jebi2420 <jebi2420@gmail.com>
2024-06-28 14:59:49 +0900

    [style] 리뷰 창 ui 작업중

--- src/pages/mypage/ReviewDialog.js
+++ src/pages/mypage/ReviewDialog.js
@@ -3,6 +3,7 @@ import { Form, Modal, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import CloudinaryUploadWidget from "../../utils/CloudinaryUploadWidget";
 import { reviewAction } from '../../action/reviewAction';
+import defaultPhoto from "../../assets/img/default_photo.png"
 
 const InitialFormData = {
   starRate: 0,
@@ -82,59 +83,63 @@ const ReviewDialog = ({ mode, showDialog, setShowDialog, searchQuery, setSearchQ
 
   return (
     <Modal show={showDialog} onHide={handleClose}>
-      <Modal.Header closeButton onClick={handleClose}>
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        <Modal.Title>리뷰</Modal.Title>
-      </Modal.Header>
-
-      {/* 별점 */}
-      <Form className="form-container" onSubmit={handleSubmit}>
-          <Form.Group as={Col} controlId="starRate">
-            <Form.Label>별점</Form.Label>
-            <Form.Control
-              onChange={handleChange}
-              type="Number"
-              placeholder="별점을 입력하세요"
-              required
-              value={formData?.starRate}
-            />
-          </Form.Group>
-
-          {/* 리뷰 이미지 */}
-          <Form.Group className="mb-3" controlId="Image" required>
-            <Form.Label>Image</Form.Label>
-            <CloudinaryUploadWidget uploadImage={uploadImage} />
-            <img
-              id="uploadedimage"
-              src={formData?.image}
-              className="upload-image mt-2"
-              alt="uploadedimage"
-              style={{ width: '30%' }}
-            ></img>
-          </Form.Group>
-
-          {/* 리뷰 내용 */}
-          <Form.Group as={Col} controlId="reviewText">
-            <Form.Label>reviewText</Form.Label>
-            <Form.Control
-              onChange={handleChange}
-              type="text"
-              placeholder="최소 15자 이상 입력해주세요"
-              required
-              value={formData?.reviewText}
-            />
-          </Form.Group>
-            
-        {mode === "new" ? (
-          <Button variant="dark" type="submit">
-            Submit
-          </Button>
-        ) : (
-          <Button variant="dark" type="submit">
-            Edit
-          </Button>
-        )}
-      </Form>
+      <div className="review_modal_container">
+        <Modal.Header className='review_modal_header' closeButton onClick={handleClose}>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <Modal.Title>리뷰</Modal.Title>
+        </Modal.Header>
+        {/* 별점 */}
+        <Form className="form-container" onSubmit={handleSubmit}>
+            <Form.Group as={Col} controlId="starRate">
+              <Form.Label>별점</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="Number"
+                placeholder="별점을 입력하세요"
+                required
+                value={formData?.starRate}
+              />
+            </Form.Group>
+            {/* 리뷰 이미지 */}
+            <Form.Group className="mb-3 review_img_area" controlId="Image" required>
+              {/* <Form.Label>Image</Form.Label> */}
+              <div class="review_img">
+                <img
+                  id="uploadedimage"
+                  src={formData?.image}
+                  className="upload-image"
+                  alt="리뷰사진"
+                  // style={{ width: '30%' }}
+                  onError={(e) => e.target.src = defaultPhoto }
+                ></img>
+              </div>
+              <div className="img_uploade_btn">
+                <CloudinaryUploadWidget uploadImage={uploadImage} />
+              </div>
+            </Form.Group>
+            {/* 리뷰 내용 */}
+            <Form.Group as={Col} controlId="reviewText">
+              <Form.Label>reviewText</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                type="text"
+                placeholder="최소 15자 이상 입력해주세요"
+                required
+                value={formData?.reviewText}
+              />
+            </Form.Group>
+        
+          {mode === "new" ? (
+            <Button variant="dark" type="submit">
+              Submit
+            </Button>
+          ) : (
+            <Button variant="dark" type="submit">
+              Edit
+            </Button>
+          )}
+        </Form>
+      </div>
     </Modal>
   );
 };
