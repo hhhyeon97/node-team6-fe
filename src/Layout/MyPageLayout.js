397dcf4a24f5de79da17584a5ee8bf298b28d7af
jebi2420 <jebi2420@gmail.com>
2024-06-30 11:54:44 +0900

    [style] 모바일 버전 마이페이지 작업중

--- src/Layout/MyPageLayout.js
+++ src/Layout/MyPageLayout.js
@@ -10,29 +10,56 @@ import "../style/css/Mypage.css";
 import "../style/css/MypageSideBar.css";
 import LinedTitle from '../component/LinedTitle';
 import UserProfile from '../component/UserProfile';
+import "../style/css/MypageMobile.css";
 
 const MyPageLayout = ({ title, cap, children }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  // const isMobile = window.matchMedia("(max-width: 480px)").matches;
+  const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 480px)").matches);
   
   // [ 유저 정보 들고오기 ] 
-  useEffect(()=>{
-    dispatch(userActions.getUser());
-  },[dispatch]);
+  // useEffect(()=>{
+  //   dispatch(userActions.getUser());
+  // },[dispatch, isMobile]);
   
+  // // [ user loading ]
+  // if (!user) {
+  //   return <div>Loading...</div>; // 또는 다른 적절한 로딩 표시
+  // }
+  const handleResize = () => {
+    setIsMobile(window.matchMedia("(max-width: 480px)").matches);
+  };
+
+  // [ 유저 정보 들고오기 ] 
+  useEffect(() => {
+    dispatch(userActions.getUser());
+
+    // 이벤트 리스너 등록
+    window.addEventListener('resize', handleResize);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [dispatch]);
+
   // [ user loading ]
   if (!user) {
     return <div>Loading...</div>; // 또는 다른 적절한 로딩 표시
   }
 
   return (
-    <>
     <Container className='wrap-container mypage-wrap-container'>
       <div class="mypage_side_bar_container">
         <UserProfile user={user} />
+        <MypageUserLevel user={user}/>
         <MypageSidebar />
       </div>
-      <div className="mypage-container">
+      {isMobile ? (
+        <></>
+      ):(
+        <div className="mypage-container">
         <section className="mypage-right">
           <MypageUserLevel user={user}/>
           <LinedTitle title={title} cap={cap}/>
@@ -40,10 +67,10 @@ const MyPageLayout = ({ title, cap, children }) => {
           {children}
           </main>
         </section>
-      </div>
+      </div>               
+      )}
     </Container>
-    </>
-  );
+);
 }
 
 export default MyPageLayout;
