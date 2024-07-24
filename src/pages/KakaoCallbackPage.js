6788cc132af6250f3adea37a4be900c66c8d5123
minsol <kth083030@naver.com>
2024-07-25 03:15:31 +0900

    [test]

--- src/pages/KakaoCallbackPage.js
+++ src/pages/KakaoCallbackPage.js
@@ -2,41 +2,31 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { userActions } from '../action/userAction';
-import LoadingText from '../component/LoadingText';
 
 const KakaoCallbackPage = () => {
   const dispatch = useDispatch();
-  const location = useLocation();
   const navigate = useNavigate();
-  const { user, loading, error } = useSelector((state) => state.user);
+  const location = useLocation();
+
+  const loginSuccess = useSelector((state) => state.user.loading); // 로그인 성공 여부를 상태에서 가져옴
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const token = urlParams.get('token');
-    if (token) {
-      localStorage.setItem('token', token);
-      dispatch(userActions.loginWithToken());
+    const code = urlParams.get('code');
+
+    if (code) {
+      dispatch(userActions.loginWithKakao(code));
     }
   }, [location.search, dispatch]);
 
   useEffect(() => {
-    if (!loading && user) {
+    if (!loginSuccess) {
+      // 로그인 성공 시 메인 페이지로 리다이렉트
       navigate('/');
     }
-  }, [loading, user, navigate]);
+  }, [loginSuccess, navigate]);
 
-  useEffect(() => {
-    if (error) {
-      console.error('로그인 에러:', error);
-      navigate('/login');
-    }
-  }, [error, navigate]);
-
-  return (
-    <div>
-      <LoadingText />
-    </div>
-  );
+  return <div>Loading...</div>;
 };
 
 export default KakaoCallbackPage;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -53,9 +53,10 @@ const AppRouter = () => {
       <Route path="/notice" element={<NoticePage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/kakao/callback" element={<KakaoCallbackPage />} />
+      {/* <Route path="/kakao/callback" element={<KakaoCallbackPage />} /> */}
       {/* <Route path="/api/auth/kakao/callback" element={<KakaoCallbackPage />} /> */}
       {/* <Route path="/api/auth/naver/callback" element={<NaverCallbackPage />} /> */}
+      <Route path="/api/auth/kakao/callback" element={<KakaoCallbackPage />} />
       <Route path="/find-password" element={<FindPasswordPage />} />
       <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
       <Route
